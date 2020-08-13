0c57e399b2513843237dd96f357553ab8a2495a9
Aidan <agoldfa7@u.rochester.edu>
2020-08-13 16:37:35 -0400

    Done

--- confine.c
+++ confine.c
@@ -17,6 +17,7 @@ int main(int argc, char **argv){
 		fprintf(stderr, "usage: %s ./binary <arg1> <arg2>...\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
+	f = fopen("confine_result.txt", "w");
 	int num_args = argc-2;
 	char *arguments[num_args+1];
 	if(argc > 2){
@@ -31,7 +32,6 @@ int main(int argc, char **argv){
 		fprintf(f, " %s ", arguments[i]);
 	}
 
-	f = fopen("confine_result.txt", "w");
 	struct rlimit *mem_rlim = malloc(sizeof(struct rlimit));
 	struct rlimit *file_rlim = malloc(sizeof(struct rlimit));;
 	mem_rlim->rlim_cur = 67108864;
@@ -46,7 +46,7 @@ int main(int argc, char **argv){
 		setrlimit(RLIMIT_AS,mem_rlim);
 		setrlimit(RLIMIT_FSIZE,file_rlim);
 		signal(SIGALRM, handler);
-		alarm(2);
+		alarm(60);
 		execve(argv[1], arguments, NULL);
 		perror("execve failed");
 		exit(EXIT_FAILURE);
@@ -57,23 +57,34 @@ int main(int argc, char **argv){
         if (wpid != -1) {
 		    if ( WIFEXITED(waitstatus) ) {
 		        int returned = WEXITSTATUS(waitstatus);
-		        printf("Exited normally with status: %d\n", returned);
+		        printf("Child process exited normally with status: %d\n", returned);
 		        fprintf(f, "\n%s", "NORMAL");
 		    }
 		    else if ( WIFSIGNALED(waitstatus) ) {
 		        int sig_num = WTERMSIG(waitstatus);
-		        printf("Exited due to receiving WIFSIGNALED signal %d\n", sig_num);
-		        fprintf(f, "\n%s", "TERMINATED");
-		        exit(128);
+		        printf("Child process exited due to receiving WIFSIGNALED signal %d\n", sig_num);
+		        if(sig_num == 14){//timeout
+		        	fprintf(f, "\n%s", "TIMEOUT");
+		        	exit(127);
+		        }
+		  		else if(sig_num == 11 || sig_num == 25){//memory || file size
+		  			fprintf(f, "\n%s", "TERMINATED");
+		  			exit(127);
+		  		}
+		        else{
+			        fprintf(f, "\n%s", "TERMINATED");
+			        exit(128);
+			    }
+		        
 		    }
 		    else if ( WIFSTOPPED(waitstatus) ) {
 		        int sig_num = WSTOPSIG(waitstatus);
-		        printf("Stopped due to receiving WIFSTOPPED signal %d\n", sig_num);
+		        printf("Child process stopped due to receiving WIFSTOPPED signal %d\n", sig_num);
 		        fprintf(f, "\n%s", "TERMINATED");
 		        exit(128);
 		    }
 		    else {
-		        printf("Other error\n");
+		        printf("Other error in child process\n");
 		        fprintf(f, "\n%s", "TERMINATED");
 		        exit(128);
 		    }
@@ -87,6 +98,4 @@ int main(int argc, char **argv){
 	fclose(f);
 	return 0;
 }
-void handler(int sig){
-	fprintf(f, "\n%s", "TIMEOUT");
-}
+void handler(int sig){}
--- test.c
+++ test.c
@@ -6,16 +6,16 @@
 void delay(int sec);
 
 int main(int argc, char **argv){
-	delay(3);
-	printf("done\n");
-	// FILE *f = fopen("output.txt", "w");
+	//delay(4);
+	//printf("done\n");
+	FILE *f = fopen("output.txt", "w");
 	// //printf("I am in test.c\narg1: %s\narg2: %s\n", argv[0],argv[1]);
-	// char *big = malloc(68108864);
-	// for(int i = 0; i<68108864; i++){
-	// 	big[i] = 'f';
-	// }
-	// fprintf(f,"%s",big);
-	// fclose(f);
+	char *big = malloc(68108864/8);
+	for(int i = 0; i<68108864/8; i++){
+		big[i] = 'f';
+	}
+	fprintf(f,"%s",big);
+	fclose(f);
 	//printf("big is size: %ld\n", sizeof(big));
 	/*buffer overflow: seg fault*/
 	// char buf[2];
